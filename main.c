c07bd9474b2085d9291ec88d555925d4fad96288
marceaubilger <marceau.bilger@orange.fr>
2024-05-20 15:39:37 +0200

    new final commit

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 
 int main() {
     int num_col,rows;
-    printf("how many columns \n");
+    printf("How many columns do you want to have: \n");
     scanf("%d",&num_col);;
-    printf("how many rows \n");
+    printf("How many rows do you want to have: \n");
     scanf("%d",&rows);;
     COLUMN **dataFrame=create_dataframe(num_col);
+    menu_filling(dataFrame,rows,num_col);
     main_menu(dataFrame,rows,num_col);
     return 0;
 }
